5adb82398aa3fdc53a2d6e69ff9b70df1e128b15
Ruggy Mango <rans2203@gmail.com>
2024-10-12 18:29:56 +0530

    Add new models

---
+++ Model/main.py
@@ -0,0 +1,24 @@
+import requests
+
+# Set up your NewsAPI key
+NEWS_API_KEY = 'a8bcc3d808424f098069111c114e3ff8'
+
+def get_news(keyword, page_size=20):
+    url = f'https://newsapi.org/v2/everything?q={keyword}&apiKey={NEWS_API_KEY}&pageSize={page_size}'
+    response = requests.get(url)
+    articles = response.json().get('articles', [])
+    
+    news = []
+    for article in articles:
+        news.append({
+            'title': article['title'],
+            'description': article['description'],
+            'url': article['url']
+        })
+    return news
+
+# Example Usage
+news_articles = get_news('technology')
+for article in news_articles:
+    print(article['title'], article['url'])
+    print()
